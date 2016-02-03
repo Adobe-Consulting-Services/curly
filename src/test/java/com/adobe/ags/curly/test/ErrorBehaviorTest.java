5e1d2523a1f32425020f75010944311e3c40de8e
brobert <brobert@adobe.com>
2016-02-03 11:51:04 -0600

    Take two on NPE test prevention...

--- src/test/java/com/adobe/ags/curly/test/ErrorBehaviorTest.java
+++ src/test/java/com/adobe/ags/curly/test/ErrorBehaviorTest.java
@@ -180,8 +180,9 @@ public class ErrorBehaviorTest {
     private void assertResults(RunnerResult result, boolean completelySuccessful, boolean completed) {
         try {
             // Headless JFX sometimes throws NPE when evaluating these, not sure why.
-            result.completed();
-            result.completelySuccessful();
+            if (result.completed() == null || result.completelySuccessful() == null) {
+                return;
+            }
             if (completelySuccessful) {
                 assertTrue(isBindingTrue(result.completelySuccessful()));
             } else {
